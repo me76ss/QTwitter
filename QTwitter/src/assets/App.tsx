16b379ff8190f6c1838a06a7cc3d1c4e1e8e1e79
me76ss <me76ss@ymail.com>
2024-06-03 22:54:14 +0330

    initial the projekt QTwitter with Vite react ts tailwind

---
+++ QTwitter/src/assets/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <p className="flex justify-center">
+
+    <h1 className="text-4xl font-bold underline">
+      Hello world!
+    </h1>
+
+    </p>
+  )
+}
