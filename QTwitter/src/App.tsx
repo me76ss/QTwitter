ac627b1d5acc4e27cc52ad15289b23036a8081bf
me76ss <me76ss@ymail.com>
2024-06-05 03:52:34 +0330

    add feed without color

--- QTwitter/src/App.tsx
+++ QTwitter/src/App.tsx
@@ -1,6 +1,7 @@
-import Login from "./components/login/login";
+// import Login from "./components/login/login";
 // import MyComponent from './components/component/component';
-import Register from './components/signup/signup';
+// import Register from './components/signup/signup';
+import Feed from './components/feed/feed';
 export default function App() {
   return (
     <div className="flex flex-col justify-center items-center">
@@ -8,8 +9,9 @@ export default function App() {
       <h1 className="text-4xl font-bold underline p-4">Hello world!</h1>
 
       <div className="my-20 flex flex-col space-y-4 ">
-        <Login></Login>
-        <Register></Register>
+        {/* <Login></Login> */}
+        {/* <Register></Register> */}
+        <Feed></Feed>
         {/* <MyComponent></MyComponent> */}
       </div>
 
---
+++ QTwitter/src/components/feed/feed.tsx
@@ -0,0 +1,38 @@
+function Feed() {
+    return (
+        <div className="flex flex-col border-2 box-content h-auto max-w-screen-md p-2 px-4 ">
+            <div className="m-4 p-2 justify-center space-x-10 flow-root">
+                <div className="flex flex-row float-left box-content">
+                    <div className="box-content p-2 py-3">profileName:</div>
+                    <div className="border-4 h-8 w-44 box-content p-2">sbsfbsfbv</div>
+                </div>
+
+                <div className="flex flex-row float-right box-content">
+                    <div className="box-content p-2 py-3">UserName:</div>
+                    <div className="border-4 h-8 w-44 box-content p-2">rbwrwrb</div>
+                </div>
+            </div>
+
+            <div className="h-auto w-auto flex flex-row m-4">
+                <div className="pr-4">tweet:</div>
+                <div className="border-4 h-72 w-full break-inside-auto px-2 py-1">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad esse
+                    beatae, provident, ex illum facere ut magnam perspiciatis voluptatibus
+                    quam, odio tempora nihil amet totam eligendi explicabo? Fugiat,
+                    dignissimos deleniti!
+                </div>
+            </div>
+                <div className="flex border-2 box-content m-4 p-2 space-x-4 justify-between">
+                    <div className="content-center">tags</div>
+                    <div className="border-4 h-8 w-44 box-content p-2">rbwrwfefrb</div>
+                    <div className="content-center" >likes</div>
+                    <div className="border-4 h-8 w-44 box-content p-2">rbwwrqrwrb</div>
+                    <div className="content-center" >comments</div>
+                    <div className="border-4 h-8 w-44 box-content p-2">rb335wrwrb</div>
+                    <div className="content-center" >reply</div>
+                    <div className="border-4 h-8 w-44 box-content p-2">rbwgsfhrwrb</div>
+                </div>
+        </div>
+    );
+}
+export default Feed;
